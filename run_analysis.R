04f78917d16427abd80073b92d791db422b39d4c
C.I.Moody <cristina.isadora.moody@gmail.com>
2016-03-01 11:20:51 -0600

    Reading in files and converting to DF.

--- run_analysis.R
+++ run_analysis.R
@@ -26,6 +26,7 @@ trainIS.dir <- paste0(train.dir, "Inertial_Signals/")
 feature <- read.table("UCI_HAR_Dataset/features.txt",
                       stringsAsFactors = F,
                       col.names = c("col.number", "feature"))
+#####
 # test
 sub_test <- read.table(file = paste0(test.dir, "subject_test.txt"),
                    stringsAsFactors = F, col.names = "subject_test")
@@ -91,3 +92,40 @@ body_gyro_z_train <- read.table(file = paste0(trainIS.dir,
 total_acc_z_train <- read.table(file = paste0(trainIS.dir,
                                              "total_acc_z_train.txt"),
                                stringsAsFactors = F)
+#####
+# Converting rawfiles into usable formats
+body_acc_x_train <- tbl_df(body_acc_x_train)
+body_acc_y_train <- tbl_df(body_acc_y_train)
+body_acc_x_train <- tbl_df(body_acc_z_train)
+#
+body_acc_x_test <- tbl_df(body_acc_x_test)
+body_acc_y_test <- tbl_df(body_acc_y_test)
+body_acc_x_test <- tbl_df(body_acc_z_test)
+##
+total_acc_x_train <- tbl_df(total_acc_x_train)
+total_acc_y_train <- tbl_df(total_acc_y_train)
+total_acc_x_train <- tbl_df(total_acc_z_train)
+#
+total_acc_x_test <- tbl_df(total_acc_x_test)
+total_acc_y_test <- tbl_df(total_acc_y_test)
+total_acc_x_test <- tbl_df(total_acc_z_test)
+##
+body_gyro_x_train <- tbl_df(body_gyro_x_train)
+body_gyro_y_train <- tbl_df(body_gyro_y_train)
+body_gyro_x_train <- tbl_df(body_gyro_z_train)
+#
+body_gyro_x_test <- tbl_df(body_gyro_x_test)
+body_gyro_y_test <- tbl_df(body_gyro_y_test)
+body_gyro_x_test <- tbl_df(body_gyro_z_test)
+##
+feature <- tbl_df(feature)
+sub_test <- tbl_df(sub_test)
+sub_train <- tbl_df(sub_train)
+X_test <- tbl_df(X_test)
+X_train <- tbl_df(X_train)
+Y_test <- tbl_df(Y_test)
+Y_train <- tbl_df(Y_train)
+
+#####
+# Combining data
+
